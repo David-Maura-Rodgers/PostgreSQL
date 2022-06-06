f215a48647955bfd9cf100a27d8fef37e4bc1e5e
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-06-06 17:01:56 +0000

    add myself to Programmer table

--- sql-crud.py
+++ sql-crud.py
@@ -91,13 +91,22 @@ tim_berners_lee = Programmer(
     famous_for="World Wide Web"
 )
 
+david_rodgers = Programmer(
+    first_name="David",
+    last_name="Rodgers",
+    gender="M",
+    nationality="Irish",
+    famous_for="Student"
+)
+
 # add each instance of our programmers to our session
 # session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margaret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+session.add(david_rodgers)
 
 # commit our session to the database
 session.commit()
